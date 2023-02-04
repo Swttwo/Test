50e5702c0c85f240af7209e56aece7cb7200d849
Swt <1438934917@qq.com>
2023-02-04 22:20:25 +0800

    Home-filx first

--- src/main/java/Test/GitTest.java
+++ src/main/java/Test/GitTest.java
@@ -1,13 +1,36 @@
 package Test;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("GitTest");
-        System.out.println("我哭死");
-        Scanner scanner=new Scanner(System.in);
-        String s = scanner.nextLine();
-        System.out.println(s);
+        int []arr= {323,233,82,4112,712321};//创建数组
+        System.out.println("排序前");
+        showArr(arr);//打印显示排序前
+        //循环实现冒泡排序
+        for(int i=0;i<arr.length-1;i++) {
+            for(int j=0;j<arr.length-i-1;j++) {
+                if(arr[j]>arr[j+1]) {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+
+            }
+        }
+        System.out.println("排序后");
+        showArr(arr);
     }
-}
+    //打印方法
+    private static void showArr(int []arr) {
+        //增强for循环打印
+        for(int a:arr) {
+            System.out.print(a+"\t");
+
+        }
+        System.out.println();
+    }
+
+    }
