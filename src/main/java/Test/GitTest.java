991cfe9c7c8e2a14b8b517bad3aa9065fc5b83ab
Swt <1438934917@qq.com>
2023-02-04 21:08:49 +0800

    Home-filx first

--- src/main/java/Test/GitTest.java
+++ src/main/java/Test/GitTest.java
@@ -1,8 +1,13 @@
 package Test;
 
+import java.util.Scanner;
+
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("GitTest");
         System.out.println("我哭死");
+        Scanner scanner=new Scanner(System.in);
+        String s = scanner.nextLine();
+        System.out.println(s);
     }
 }
